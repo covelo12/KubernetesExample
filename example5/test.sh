4e9f8fcd52a57e3c3ab22905d330646e8f85cc25
Ricardo Covelo <ricardocovelo11@ua.pt>
2026-01-21 10:03:58 +0000

    udate

---
+++ example5/test.sh
@@ -0,0 +1,28 @@
+#When a deploymenet is used, it keeps creating pods
+destroy_pods() {
+  kubectl delete pods -n default --all
+}
+#You need to delete the deployment or it will not stop creating pods
+destroy_deploy() {
+  kubectl delete pods -n default --all
+}
+#This time logs won't be transmited but putted into a logs folder
+create_tester() {
+  kubectl run tester --image=busybox -it --rm -- sh
+}
+#Both pods share this folder and both will write here
+get_logs() {
+  # Get the name of one pod from the echo deployment
+  POD=$(kubectl get pods -l app=echo -o jsonpath='{.items[0].metadata.name}')
+  # Show its logs
+  kubectl exec "$POD" -- cat /logs/echo.log
+}
+get_resources() {
+
+  POD=$(kubectl get pods -l app=echo -o jsonpath='{.items[0].metadata.name}')
+
+  #kubectl top pod "$POD"
+  kubectl describe pod "$POD"
+}
+$1
+### run this inside the pod
---
+++ example6/README.md
@@ -0,0 +1,16 @@
+# Resources
+
+Resources aren't infinite, specially if you are on prem. And not all applications are the same, some are more critical than others.
+Ensuring that all applications have just enough resources to run and not more is complicated to say the least.
+
+The first step to that is to stablish limits on:
+
+- How much does an application minimum requirements to be able to run (**Requests**)
+- How much does an application maximum consumption should be for the well-being of the cluster(**Limits**)
+
+1- Run deployment
+2- Run the `get_resources`
+
+There is two ways we can program K8s applications to handle more load:
+**Vertical Scaling**: Allowing a pod to consume more resources to handle more load.
+**Horizontal Scaling**: Creating more pods as the load increases.
